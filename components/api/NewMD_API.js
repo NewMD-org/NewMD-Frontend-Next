8b4e9f7df6eb3097e5a8feed5f9aef5d748eb5d1
Alex <anonymousaaaa41414141@gmail.com>
2022-12-20 18:01:38 +0800

    Fix bug that cause error when checking api availability
    Fix
    -Function of checking response message has been changed to fit backend changes

--- components/api/NewMD_API.js
+++ components/api/NewMD_API.js
@@ -17,7 +17,7 @@ async function testAPI() {
             }
         });
 
-        if (cloud0.data === ("Service is running")) {
+        if (cloud0.data.includes("Service is up")) {
             status0 = true;
             availableURL.push(cloud0.config?.url.replace("/ping", ""));
             console.log("Refresh NewMD_API: cloud0 available");
@@ -34,7 +34,7 @@ async function testAPI() {
             timeout: 2 * 1000
         });
 
-        if (cloud1.data === "Service is running") {
+        if (cloud1.data.includes("Service is up")) {
             status1 = true;
             availableURL.push(cloud1.config?.url.replace("/ping", ""));
             console.log("Refresh NewMD_API: cloud1 available");
