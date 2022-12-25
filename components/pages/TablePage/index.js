66583ad6ff718b6d59be14b6294facfbc451bd9c
Alex Liao <anonymousaaaa41414141@gmail.com>
2022-12-25 08:28:58 +0800

    Modify code to fit new feature

--- components/pages/TablePage/index.js
+++ components/pages/TablePage/index.js
@@ -1,22 +1,24 @@
 import { useState } from "react";
 
 import NavbarTop from "./components/NavbarTop";
-import { ClassesTable } from "./components/ClassesTable";
+import ClassesTable from "./components/ClassesTable";
 import Loader from "./components/Loader";
+import Snow from "./components/Snow";
 
 import styles from "./Table.module.css";
-// import snowdrift from "./snowdrift.png";
 
 
 export default function TablePage({ state, authorization }) {
     const [isLoading, setIsLoading] = useState(true);
+    const [enableSnow, setEnableSnow] = useState(true);
 
     return (
         <>
             <div className={styles.background}>
                 {isLoading ? <Loader /> : <></>}
-                <NavbarTop state={state} authorization={authorization} />
-                <ClassesTable isLoading={isLoading} setIsLoading={setIsLoading} state={state} authorization={authorization} />
+                <NavbarTop state={state} authorization={authorization} enableSnow={enableSnow} setEnableSnow={setEnableSnow} />
+                <ClassesTable isLoading={isLoading} setIsLoading={setIsLoading} state={state} authorization={authorization} enableSnow={enableSnow} />
+                {enableSnow ? <Snow /> : <></>}
             </div>
         </>
     );
