dd1bc3a9b86d4c98b88a2317ea387ad596cd8655
OnCloud <anonymousaaaa41414141@gmail.com>
2023-03-27 04:40:05 +0800

    Add PWA support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
 };
-
 module.exports = nextConfig;
+
+const withPWA = require("next-pwa")({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+});
+module.exports = withPWA();
