9a0e23b8a81b2164c62ec507bc37b68ee01f18fd
Alex Liao <anonymousaaaa41414141@gmail.com>
2023-04-28 00:55:13 +0800

    Prepare to switch Ui to Mantine

--- components/pages/TablePage/components/NavbarTop/index.js
+++ components/pages/TablePage/components/NavbarTop/index.js
@@ -2,6 +2,7 @@ import { useRef, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import cookie from "react-cookies";
+import { Switch } from "@mantine/core";
 import styles from "./NavbarTop.module.css";
 
 import NewMD_API from "../../../../api/NewMD_API";
@@ -11,8 +12,8 @@ import Attention from "./components/Attention";
 
 export default function NavbarTop({ state, authorization, enableChristmas, _setEnableChristmas }) {
     const router = useRouter();
-    const saveDataInput = useRef(null);
     const setSnowInput = useRef(null);
+    const saveDataInput = useRef(null);
     const menu = useRef(null);
 
     const [userDataStatus, setUserDataStatus] = useState(state["userDataStatus"].toString());
@@ -32,33 +33,57 @@ export default function NavbarTop({ state, authorization, enableChristmas, _setE
                 </label>
                 <ul className={styles.menu}>
                     <li>
-                        <div className={styles.saveData} style={{ cursor: "pointer" }} onClick={() => saveDataInput.current.click()}>
+                        <div className={styles.saveData} style={{ cursor: "pointer" }} onClick={() => setSnowInput.current.click()}>
                             Christmas Decoration
-                            <div className={join(styles.switch, "noselect", "pretty", "p-switch", "p-fill")}>
-                                <input type="checkbox" name="Enable Christmas Decoration" ref={saveDataInput} checked={enableChristmas} onChange={(e) => _setEnableChristmas(e.target.checked)} />
+                            {/* <div className={join(styles.switch, "noselect", "pretty", "p-switch", "p-fill")}>
+                                <input type="checkbox" name="Enable Christmas Decoration" ref={setSnowInput} checked={enableChristmas} onChange={(e) => _setEnableChristmas(e.target.checked)} />
                                 <div className={"state p-success"}>
                                     <label></label>
                                 </div>
-                            </div>
+                            </div> */}
+
                         </div>
                     </li>
                     <li>
-                        <div className={styles.saveData} style={isLoading ? { cursor: "not-allowed", color: "GrayText" } : { cursor: "pointer" }} onClick={() => setSnowInput.current.click()}>
-                            {isLoading ? (
-                                state["userDataStatus"] === "true" ? (
-                                    <>Deleting</>
-                                ) : (
-                                    <>Saving</>
-                                )
-                            ) : (
-                                <>Save Data</>
-                            )}
+                        {/* <div className={styles.saveData}  onClick={() => saveDataInput.current.click()}>
                             <div className={join(styles.switch, "noselect", "pretty", "p-switch", "p-fill")}>
-                                <input type="checkbox" name="userDataStatus" ref={setSnowInput} checked={userDataStatus === "true"} disabled={isLoading} onChange={(e) => userDataStatusChange(e.target.checked)} />
+                                <input type="checkbox" name="userDataStatus" ref={saveDataInput} checked={userDataStatus === "true"} disabled={isLoading} onChange={(e) => userDataStatusChange(e.target.checked)} />
                                 <div className={"state p-success"}>
                                     <label></label>
                                 </div>
                             </div>
+                            <Switch
+                                name="userDataStatus"
+                                labelPosition="left"
+                                label={isLoading ? (state["userDataStatus"] === "true" ? "Deleting" : "Saving") : "Save Data"}
+                                size="md"
+                                color="green"
+                                style={isLoading ? { cursor: "not-allowed", color: "GrayText" } : { cursor: "pointer" }}
+
+                                ref={saveDataInput}
+                                checked={userDataStatus === "true"}
+                                disabled={isLoading}
+                                onChange={(e) => userDataStatusChange(e.target.checked)}
+                            />
+                        </div> */}
+                        <div
+                            className={styles.saveData}
+                            style={isLoading ? { cursor: "not-allowed", color: "GrayText" } : { cursor: "pointer" }}
+                            onClick={() => saveDataInput.current.click()}
+                        >
+                            <Switch
+                                name="userDataStatus"
+                                labelPosition="left"
+                                label={isLoading ? (state["userDataStatus"] === "true" ? "Deleting" : "Saving") : "Save Data"}
+                                size="md"
+                                color="green"
+                                style={{ zIndex: "-1", userSelect: "none" }}
+
+                                ref={saveDataInput}
+                                checked={userDataStatus === "true"}
+                                disabled={isLoading}
+                                onChange={(e) => userDataStatusChange(e.target.checked)}
+                            />
                         </div>
                     </li>
                     <li>
@@ -78,24 +103,24 @@ export default function NavbarTop({ state, authorization, enableChristmas, _setE
         try {
             console.log("Delete user data : start");
             const t0 = performance.now();
-            const response = await new NewMD_API(5).delete(token);
-            if (response.status === 200) {
-                setUserDataStatus("false");
-                const t1 = performance.now();
-                console.log(`Delete user data : success (took ${Math.round(t1 - t0) / 1000} seconds)`);
-                sessionStorage.setItem("userDataStatus", "false");
-                router.replace({
-                    pathname: "/table",
-                    query: {
-                        "userDataStatus": "false",
-                        "tableData": state["tableData"],
-                        "year": state["year"]
-                    }
-                }, "/table");
-            }
-            else {
-                throw Error("Joanne is smart");
-            };
+            // const response = await new NewMD_API(5).delete(token);
+            // if (response.status === 200) {
+            //     setUserDataStatus("false");
+            //     const t1 = performance.now();
+            //     console.log(`Delete user data : success (took ${Math.round(t1 - t0) / 1000} seconds)`);
+            //     sessionStorage.setItem("userDataStatus", "false");
+            //     router.replace({
+            //         pathname: "/table",
+            //         query: {
+            //             "userDataStatus": "false",
+            //             "tableData": state["tableData"],
+            //             "year": state["year"]
+            //         }
+            //     }, "/table");
+            // }
+            // else {
+            //     throw Error("Joanne is smart");
+            // };
         }
         catch (err) {
             setUserDataStatus("true");
