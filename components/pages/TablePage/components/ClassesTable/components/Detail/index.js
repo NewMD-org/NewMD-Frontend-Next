1f783126a1f3f9119b6909072452423caccf1a3a
Alex Liao <anonymousaaaa41414141@gmail.com>
2023-05-05 14:09:58 +0800

    Update detail modal
    Changed
    -Copy icon
    
    Add
    -Tooltip

--- components/pages/TablePage/components/ClassesTable/components/Detail/index.js
+++ components/pages/TablePage/components/ClassesTable/components/Detail/index.js
@@ -1,5 +1,5 @@
 import { useCallback, useEffect, useState } from "react";
-import { Modal } from "@mantine/core";
+import { Modal, Tooltip } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import styles from "./Detail.module.css";
 
@@ -59,70 +59,110 @@ export default function Detail({ showDetail, setShowDetail, setDetail, detail, s
     }, [setDetail, setShowDetail]);
 
     return (
-        <>
-            <Modal
-                centered
-                className={styles.modal_container}
-                overlayProps={{
-                    color: "#000000",
-                    opacity: 0.55,
-                    blur: 3,
-                }}
-                transitionProps={{
-                    transition: "pop",
-                    duration: 200,
-                    timingFunction: "linear"
-                }}
+        <Modal
+            centered
+            className={styles.modal_container}
+            overlayProps={{
+                color: "#000000",
+                opacity: 0.55,
+                blur: 3,
+            }}
+            transitionProps={{
+                transition: "pop",
+                duration: 200,
+                timingFunction: "linear"
+            }}
 
-                opened={opened}
-                onClose={closeModal}
-            >
-                <h1 className={styles.modal__title}>{detail["name"]}</h1>
-                <div className={styles.field_container}>
-                    {isLoading ? (
-
-                        <>
-                            <div className={styles.spinner_container}>
-                                <div className={styles.spinner}></div>
-                            </div>
-                            <div className={styles.text_area}>
-                                <p className={styles.title}>Waiting for too long ?</p>
-                                <p className={styles.content}>Try enabling the &quot;Save Data&quot; option !</p>
-                            </div>
-                        </>
-                    ) : (
-                        <>
-                            <p className={styles.modal__text}>Google Meet</p>
-                            <div className={styles.form}>
-                                {message.meet === "none" ? (
-                                    <>
-                                        <p className={styles.field_none}>{message.meet}</p>
-                                    </>
-                                ) : (
-                                    <>
-                                        <a className={join(styles.field, styles.meet, "yesselect")} href={message.meet} target="_blank" rel="noreferrer">{message.meet}</a>
-                                        <span title="Copy" onClick={() => { copyToClipboard(message.meet); setCopySuccess0(true); }} onMouseLeave={() => setCopySuccess0(false)}>{copySuccess0 ? <>&#x2714;</> : <>&#x1F4CB;</>}</span>
-                                    </>
-                                )}
-                            </div>
-                            <p className={styles.modal__text}>Classroom Code</p>
-                            <div className={styles.form}>
-                                {message.classroom === "none" ? (
-                                    <>
-                                        <p className={styles.field_none}>{message.classroom}</p>
-                                    </>
-                                ) : (
-                                    <>
-                                        <p className={join(styles.field, styles.classroom, "yesselect")}>{message.classroom}</p>
-                                        <span title="Copy" onClick={() => { copyToClipboard(message.classroom); setCopySuccess1(true); }} onMouseLeave={() => setCopySuccess1(false)}>{copySuccess1 ? <>&#x2714;</> : <>&#x1F4CB;</>}</span>
-                                    </>
-                                )}
-                            </div>
-                        </>
-                    )}
-                </div>
-            </Modal >
-        </>
+            opened={opened}
+            onClose={closeModal}
+        >
+            <h1 className={styles.modal__title}>{detail["name"]}</h1>
+            <div className={styles.field_container}>
+                {isLoading ? <>
+                    <div className={styles.spinner_container}>
+                        <div className={styles.spinner}></div>
+                    </div>
+                    <div className={styles.text_area}>
+                        <p className={styles.title}>Waiting for too long ?</p>
+                        <p className={styles.content}>Try enabling the &quot;Save Data&quot; option !</p>
+                    </div>
+                </> : <>
+                    <p className={styles.modal__text}>Google Meet</p>
+                    <div className={styles.form}>
+                        {message.meet === "none" ? <>
+                            <p className={styles.field_none}>{message.meet}</p>
+                        </> : <>
+                            <Tooltip
+                                label="Open in new tab"
+                            >
+                                <a className={join(styles.field, styles.meet, "yesselect")} href={message.meet} target="_blank" rel="noreferrer">{message.meet}</a>
+                            </Tooltip>
+                            <Tooltip
+                                label="Copy"
+                            >
+                                <span
+                                    onClick={() => {
+                                        copyToClipboard(message.meet);
+                                        setCopySuccess0(true);
+                                    }}
+                                    onMouseLeave={() => setCopySuccess0(false)}
+                                >
+                                    {copySuccess0 ? <>
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-clipboard-check" width="24" height="24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                            <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2"></path>
+                                            <path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z"></path>
+                                            <path d="M9 14l2 2l4 -4"></path>
+                                        </svg>
+                                    </> : <>
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-clipboard" width="24" height="24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                            <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2"></path>
+                                            <path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z"></path>
+                                        </svg>
+                                    </>}
+                                </span>
+                            </Tooltip>
+                        </>}
+                    </div>
+                    <p className={styles.modal__text}>Classroom Code</p>
+                    <div className={styles.form}>
+                        {message.classroom === "none" ? <>
+                            <p className={styles.field_none}>{message.classroom}</p>
+                        </> : <>
+                            <p className={join(styles.field, styles.classroom, "yesselect")}>{message.classroom}</p>
+                            <Tooltip
+                                label="Copy"
+                            >
+                                <span
+                                    title="Copy"
+                                    onClick={() => {
+                                        copyToClipboard(message.classroom);
+                                        setCopySuccess1(true);
+                                    }}
+                                    onMouseLeave={() => setCopySuccess1(false)}
+                                >
+                                    {copySuccess1 ? <>
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-clipboard-check" width="24" height="24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                            <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2"></path>
+                                            <path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z"></path>
+                                            <path d="M9 14l2 2l4 -4"></path>
+                                        </svg>
+                                    </> : <>
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-clipboard" width="24" height="24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                            <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                            <path d="M9 5h-2a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-12a2 2 0 0 0 -2 -2h-2"></path>
+                                            <path d="M9 3m0 2a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v0a2 2 0 0 1 -2 2h-2a2 2 0 0 1 -2 -2z"></path>
+                                        </svg>
+                                    </>}
+                                </span>
+                            </Tooltip>
+                        </>}
+                    </div>
+                </>}
+            </div>
+        </Modal >
     );
 
     async function viewVT(year, classID) {
