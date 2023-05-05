1af36b740a19ef32cb779c23203c9939bc5d066a
Alex Liao <anonymousaaaa41414141@gmail.com>
2023-05-05 14:08:53 +0800

    Add logout icon

--- components/pages/TablePage/components/NavbarTop/index.js
+++ components/pages/TablePage/components/NavbarTop/index.js
@@ -73,6 +73,11 @@ export default function NavbarTop({ state, authorization, decoration, setDecorat
                             <Link href="/logout">
                                 Logout
                             </Link>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-logout" width="24" height="24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2"></path>
+                                <path d="M7 12h14l-3 -3m0 6l3 -3"></path>
+                            </svg>
                         </div>
                     </li>
                 </ul>
