74e90d161a3f2f5efda0a64d4535ad0400450b39
OnCloud <anonymousaaaa41414141@gmail.com>
2023-07-16 02:26:06 +0800

    Fix syntax error

--- components/pages/TablePage/components/NavbarTop/index.js
+++ components/pages/TablePage/components/NavbarTop/index.js
@@ -73,7 +73,7 @@ export default function NavbarTop({ state, authorization, decoration, setDecorat
                             <span>
                                 Logout
                             </span>
-                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-logout" width="24" height="24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-logout" width="24" height="24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">
                                 <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                                 <path d="M14 8v-2a2 2 0 0 0 -2 -2h-7a2 2 0 0 0 -2 2v12a2 2 0 0 0 2 2h7a2 2 0 0 0 2 -2v-2"></path>
                                 <path d="M7 12h14l-3 -3m0 6l3 -3"></path>
