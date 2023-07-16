059c1701b01c180393f95a06b0c890c058440f55
OnCloud <anonymousaaaa41414141@gmail.com>
2023-07-17 05:40:09 +0800

    Add option to enter password, add error message block

--- components/pages/LoginPage/index.js
+++ components/pages/LoginPage/index.js
@@ -1,7 +1,8 @@
 import { useRef, useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
-import { Button, Checkbox } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { Button, Checkbox, Dialog, Text } from "@mantine/core";
 import jwt_decode from "jwt-decode";
 import cookie from "react-cookies";
 import styles from "./LoginPage.module.css";
@@ -9,30 +10,13 @@ import styles from "./LoginPage.module.css";
 import NewMD_API from "../../api/NewMD_API";
 
 import InstallPWA from "./components/InstallPWA";
+import AccountSwitch from "./components/AccountSwitch";
 
-
-function join(...array) {
-    return array.join(" ");
-}
-
-function isValidAuth(JWT) {
-    try {
-        return [
-            typeof (JWT) === "string",
-            JSON.stringify(Object.keys(jwt_decode(JWT))) === JSON.stringify(["userID", "userPWD", "rememberMe", "iat", "exp"]),
-            jwt_decode(JWT).exp >= (new Date().getTime() / 1000)
-        ].every(test => test === true);
-    }
-    catch (err) {
-        return false;
-    };
-}
 
 export default function LoginPage() {
     const IDRef = useRef();
     const PWDRef = useRef();
-    const errRef = useRef();
-    const signinRef = useRef();
+    const signInRef = useRef();
     const router = useRouter();
 
     const [ID, setID] = useState("");
@@ -40,6 +24,9 @@ export default function LoginPage() {
     const [rememberMe, setRememberMe] = useState("");
     const [errMsg, setErrMsg] = useState("");
     const [isLoading, setLoading] = useState(false);
+    const [oldAccount, setOldAccount] = useState(false);
+
+    const [opened, { open: showErrorMessage, close: closeErrorMessage }] = useDisclosure(false);
 
     useEffect(() => {
         IDRef.current.focus();
@@ -55,9 +42,113 @@ export default function LoginPage() {
 
     useEffect(() => {
         setErrMsg("");
-    }, [ID, PWD, rememberMe]);
+        closeErrorMessage();
+    }, [ID, PWD, closeErrorMessage, rememberMe]);
 
-    const handleSubmit = async (e) => {
+    useEffect(() => {
+        setPWD(oldAccount ? "" : ID);
+        IDRef.current.focus();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [oldAccount]);
+
+    return (
+        <>
+            <div className={join(styles.background, "noselect")}>
+                <Image className={styles.backgroundImage} alt="background image" src="/background.svg" draggable="false" fill />
+                <div className={styles.centerContainer}>
+                    <div className={styles.logo}>
+                        <Image className={styles.img} alt="logo" src="/logo.svg" width={709} height={225} draggable="false" priority />
+                    </div>
+                    <div className={styles.center}>
+                        <AccountSwitch
+                            leftText={"I don't have password"}
+                            rightText={"I have password"}
+
+                            checked={oldAccount}
+                            onChange={(e) => setOldAccount(e.target.checked)}
+                        />
+                        <form
+                            className={styles.form}
+                            style={{
+                                "--textfieldPWDHeight": oldAccount ? "var(--textfieldHeight)" : "0",
+                                "--movement": oldAccount ? "0" : "var(--textfieldHeight)"
+                            }}
+                            onSubmit={handleSubmit}
+                        >
+                            <div className={styles.textfield}>
+                                <input
+                                    type="text"
+                                    name="ID"
+                                    placeholder="Username"
+                                    autoComplete="username"
+
+                                    ref={IDRef}
+                                    value={ID}
+                                    onChange={(e) => setID(e.target.value)}
+                                />
+                                <span className={styles.text_focusEffect} />
+                            </div>
+                            <div className={join(styles.textfield, styles.textfieldPWD)}>
+                                <input
+                                    type="password"
+                                    name="PWD"
+                                    placeholder="Password"
+                                    autoComplete="current-password"
+
+                                    ref={PWDRef}
+                                    value={PWD}
+                                    onChange={(e) => setPWD(e.target.value)}
+                                />
+                                <span className={styles.text_focusEffect} />
+                            </div>
+                            <Checkbox
+                                label="Remember me for 7 days"
+                                color="green"
+                                size="md"
+                                style={{ cursor: "pointer" }}
+                                className={styles.rememberMe}
+
+                                ref={signInRef}
+                                checked={rememberMe === "true"}
+                                onChange={(e) => setRememberMe(e.currentTarget.checked ? "true" : "false")}
+                                autoComplete="on"
+                            />
+                            <Button
+                                radius="xl"
+                                size="xl"
+                                className={styles.signIn}
+                                style={{ cursor: isLoading ? "not-allowed" : "pointer" }}
+                                loading={isLoading}
+
+                                onClick={handleSubmit}
+                            >
+                                Sign in
+                            </Button>
+                        </form>
+                    </div>
+                </div>
+                <InstallPWA />
+            </div>
+            <Dialog
+                withCloseButton
+                withBorder
+                size="lg"
+                radius="md"
+                position={{ top: 20, left: 20 }}
+                className={styles.errorMessage}
+                cur
+
+                opened={opened}
+                onClose={closeErrorMessage}
+            >
+                <Text weight={500}>
+                    {errMsg}
+                </Text>
+            </Dialog>
+        </>
+    );
+
+    async function handleSubmit(e) {
         console.log("Manual login : start");
         const t0 = performance.now();
         console.log(`ID : ${ID}\nPWD : ${PWD}\nrememberMe : ${rememberMe}`);
@@ -65,13 +156,10 @@ export default function LoginPage() {
         setLoading(true);
 
         try {
-            const response = await new NewMD_API(20).login(ID, PWD, rememberMe);
+            const response = await new NewMD_API(20).login(ID, oldAccount ? PWD : ID, rememberMe);
             if (response["error"] === false) {
                 cookie.save("navigate", "true", { path: "/", maxAge: 60 * 60 * 24 * 7 });
                 localStorage.setItem("authorization", response["data"]["authorization"]);
-                setID("");
-                setPWD("");
-                setRememberMe("");
                 const t1 = performance.now();
                 console.log(`Manual login : success (took ${Math.round(t1 - t0) / 1000} seconds)`);
                 return router.replace({
@@ -88,11 +176,9 @@ export default function LoginPage() {
                 else if (response["message"] === "Missing Password") {
                     PWDRef.current.focus();
                 }
-                else {
-                    errRef.current.focus();
-                }
 
                 setErrMsg(response["message"]);
+                showErrorMessage();
                 console.log(`Manual login : ${response["message"]}`);
                 console.log("Manual login : failed");
                 console.log("Clear local storage, session storage and cookie");
@@ -103,67 +189,31 @@ export default function LoginPage() {
         }
         catch (err) {
             setErrMsg("Manual login : unexpected error");
+            showErrorMessage();
             console.log("Remove cookie : navigate");
             cookie.remove("navigate");
-            errRef.current.focus();
         };
 
         return setLoading(false);
     };
-
-    return (
-        <>
-            <div className={join(styles.background, "noselect")}>
-                <Image className={styles.backgroundImage} alt="background image" src="/background.svg" draggable="false" fill />
-                <div className={styles.centerContainer}>
-                    <div className={styles.logo}>
-                        <Image className={styles.img} alt="logo" src="/logo.svg" width={709} height={225} draggable="false" priority />
-                    </div>
-                    <div className={styles.center}>
-                        <p ref={errRef} className={errMsg ? styles.errmsg : styles.offscreen} aria-live="assertive">{errMsg}</p>
-                        <form className={styles.form} onSubmit={handleSubmit}>
-                            <div className={styles.textfield}>
-                                <input type="text" name="ID" ref={IDRef} value={ID} onChange={(e) => setID(e.target.value)} placeholder="Username" autoComplete="username" />
-                                <span className={styles.text_focusEffect}></span>
-                            </div>
-                            <div className={styles.textfield}>
-                                <input type="password" name="PWD" ref={PWDRef} value={PWD} onChange={(e) => setPWD(e.target.value)} placeholder="Password" autoComplete="current-password" />
-                                <span className={styles.text_focusEffect}></span>
-                            </div>
-                            <div
-                                className={styles.rememberme}
-                                onClick={() => signinRef.current.click()}
-                            >
-                                <Checkbox
-                                    label="Remember me for 7 days"
-                                    color="green"
-                                    size="md"
-                                    style={{ pointerEvents: "none" }}
-
-                                    ref={signinRef}
-                                    checked={rememberMe === "true"}
-                                    onChange={(e) => setRememberMe(e.currentTarget.checked ? "true" : "false")}
-                                    autoComplete="on"
-                                />
-                            </div>
-                            <div className={styles.centerDiv}>
-                                <Button
-                                    radius="xl"
-                                    size="xl"
-                                    className={styles.signin}
-                                    style={{ cursor: isLoading ? "not-allowed" : "pointer" }}
-                                    loading={isLoading}
-
-                                    onClick={handleSubmit}
-                                >
-                                    Sign in
-                                </Button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <InstallPWA />
-            </div>
-        </>
-    );
 };
+
+/**
+ * Join multiple classes
+ */
+function join(...classes) {
+    return classes.join(" ");
+}
+
+function isValidAuth(JWT) {
+    try {
+        return [
+            typeof (JWT) === "string",
+            JSON.stringify(Object.keys(jwt_decode(JWT))) === JSON.stringify(["userID", "userPWD", "rememberMe", "iat", "exp"]),
+            jwt_decode(JWT).exp >= (new Date().getTime() / 1000)
+        ].every(test => test === true);
+    }
+    catch (err) {
+        return false;
+    };
+}
