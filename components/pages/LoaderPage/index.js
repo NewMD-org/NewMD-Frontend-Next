d64086c3010d6c609b82729f92f3f66451f8684c
Alex Liao <anonymousaaaa41414141@gmail.com>
2023-07-24 15:52:31 +0800

    localize the loading components into Chinese

--- components/pages/LoaderPage/index.js
+++ components/pages/LoaderPage/index.js
@@ -6,12 +6,13 @@ import styles from "./Loader.module.css";
 
 
 const messages = [
-    "Your password stored in Mingdao's server isn't hashed!",
-    "It dosn't matter if you input your account & password with capital letter.",
-    "We won't store your data unless you enable \"Save Data\" option.",
-    "We use MongoDB to store your data.",
-    "This page is built by ReactJS.",
-    "We will update your data at 24:00 every day."
+    "啟用「儲存資料」的選項可以極大的縮減查詢課表的時間",
+    "你可以讓課表下雪喔",
+    "身份證字號及密碼是不分大小寫的",
+    "我們永遠不會儲存您的任何資料直到你啟用了「儲存資料」的選項",
+    "我們使用了 MongoDB 作為資料庫儲存課表",
+    "這個網站是用 Next.js 作為主要框架設計的",
+    "我們會在每天的早上 8:00 更新每一個儲存的課表"
 ];
 
 export default function Loader({ retryTimes }) {
@@ -28,18 +29,18 @@ export default function Loader({ retryTimes }) {
             </Head>
             <div className={styles.background}>
                 <div className={styles.text_area}>
-                    <p className={styles.title}>Do you know ?</p>
+                    <p className={styles.title}>你知道嗎 ?</p>
                     <p className={styles.content}>{message}</p>
                     {retryTimes > 0 ? (
                         <div className={styles.retry_times}>
-                            Retried {retryTimes} times
+                            嘗試登入 {retryTimes} 次
                             <span>|</span>
-                            <Link href="/login" onClick={removeCookie}>Back to login</Link>
+                            <Link href="/login" onClick={removeCookie}>回到登入畫面</Link>
                         </div>
                     ) : <></>}
                 </div>
                 <div className={styles.bouncing_loader}>
-                    <p>Loading</p>
+                    <p>正在載入</p>
                     <div></div>
                     <div></div>
                     <div></div>
