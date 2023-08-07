5a089130125bfca151150ccf5f6b82d258ddbe64
Alex Liao <anonymousaaaa41414141@gmail.com>
2023-08-07 09:50:13 +0800

    Update open graph image

--- pages/_document.js
+++ pages/_document.js
@@ -26,20 +26,14 @@ export default function Document() {
                 <meta name="twitter:description" content="想快速看到明道課表嗎？ NewMD 課表提供更快速、更穩定的查詢，並擁有更好看的介面。快來試試吧！" />
                 <meta name="twitter:site" content="@" />
                 <meta name="twitter:creator" content="@" />
-                <meta name="twitter:image" content="https://i.imgur.com/tPYMyLP.png" />
+                <meta name="twitter:image" content="https://i.ibb.co/F4jtbXJ/Open-Graph.png" />
 
                 {/* <!-- Open Graph --> */}
                 <meta property="og:url" content="https://newmd.eu.org" />
                 <meta property="og:title" content="NewMD Timetable - Beautiful & Faster version of MDHS timetable" />
                 <meta property="og:type" content="website" />
                 <meta property="og:description" content="想快速看到明道課表嗎？ NewMD 課表提供更快速、更穩定的查詢，並擁有更好看的介面。快來試試吧！" />
-                <meta property="og:image" content="https://i.imgur.com/tPYMyLP.png" />
-
-                {/* eslint-disable-next-line @next/next/google-font-display */}
-                {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat" /> */}
-                {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pretty-checkbox@3.0/dist/pretty-checkbox.min.css" /> */}
-                {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css"
-                    integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossOrigin="anonymous" /> */}
+                <meta property="og:image" content="https://i.ibb.co/F4jtbXJ/Open-Graph.png" />
             </Head>
             <body>
                 <Main />
